fc25f764553c1acf46415e167e0f3276ab3e2427
Ubuntu <ubuntu@ip-172-31-18-27.eu-central-1.compute.internal>
2017-11-10 18:55:14 +0000

    a

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-  res.send('HELLOOO!')
+  res.send('HELLAAA!')
 })
 app.listen(3000, () => console.log('Server running on port 3000'))
